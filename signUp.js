9492e0ba6cdf561cbd3754d4d28e262d9f6993cd
Victor Chinedu <victorwhite590@gmail.com>
2025-08-18 12:24:12 +0100

    fixing registration failed err due to wrong spelling of message

--- signUp.js
+++ signUp.js
@@ -1,8 +1,7 @@
 let current = 0;
 let steps = document.querySelectorAll(".form-page");
 let nextBtns = document.querySelectorAll(".next-btn");
-const togglePassword = document.getElementById("togglePassword")
-
+const togglePassword = document.getElementById("togglePassword");
 
 nextBtns.forEach((btn) => {
   btn.addEventListener("click", nextStep);
@@ -34,75 +33,58 @@ function backStep() {
   }
 }
 
+function toggleIconForPassword(toggleId, inputId) {
+  const toggle = document.getElementById(toggleId);
+  const input = document.getElementById(inputId);
 
-function toggleIconForPassword(toggleId, inputId){
-    const toggle = document.getElementById(toggleId);
-    const input = document.getElementById(inputId);
-
-    toggle.addEventListener("click", () => {
-        input.type = input.type === "password" ? "text" : "password"
-    })
+  toggle.addEventListener("click", () => {
+    input.type = input.type === "password" ? "text" : "password";
+  });
 }
 
 toggleIconForPassword("togglePassword", "password");
-toggleIconForPassword("togglePassword2", "password2")
-
-
-
+toggleIconForPassword("togglePassword2", "password2");
 
 // Handles form submission
-document.addEventListener("DOMContentLoaded", () =>{
+document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("multiform");
   const loader = document.getElementById("loader");
   form.addEventListener("submit", async (e) => {
     e.preventDefault();
-
-  
-
 
     const email = e.target.email.value.trim();
     const fullName = e.target.fullName.value.trim();
     const password = e.target.password.value.trim();
     const password2 = e.target.password2.value.trim();
 
-
-    
-
-
-    if( password !== password2){
-      alert("password do not match")
+    if (password !== password2) {
+      alert("password do not match");
       return;
     }
 
-    try{
+    try {
+      loader.style.display = "flex";
 
-       loader.style.display = "flex";
-
-      const req = await fetch ("https://form-handling-4.onrender.com/register", 
-         {
-          method: "POST",
-          headers:{
-            "content-type" : "application/json"
-          },
-          body: JSON.stringify({email, password, fullName})
-         }
-      );
+      const req = await fetch("https://form-handling-4.onrender.com/register", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ email, password, fullName }),
+      });
       const data = await req.json();
-      if (data.mesage === "User registered successfully"){
-        alert("Registration Sucessful")
-        window.location.href = "profile.html"
-      }else{
-       alert(data.mesaage || "Registration Failed, please try again later")
+      if (data.message === "User registered successfully") {
+        alert("Registration Successful");
+        window.location.href = "profile.html";
+      } else {
+        alert(data.message || "Registration Failed, please try again later");
       }
-    }
-    catch(err){
-      console.log(err.message)
-      alert("something went wrong. Please try again")
-    }
-    finally {
+    } catch (err) {
+      console.log(err.message);
+      alert("something went wrong. Please try again");
+    } finally {
       // Hide loader
       loader.style.display = "none";
-    }  
-    
-  })
-})
+    }
+  });
+});
